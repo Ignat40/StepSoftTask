ac89ba60ce8bce4a9666b17a053b813ea7d088dc
Ignat Bojinov <ignatbojinov@gmail.com>
2024-05-28 15:18:44 +0200

    Update UI after registration

--- webApp/resources/views/home.blade.php
+++ webApp/resources/views/home.blade.php
@@ -3,18 +3,35 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ Auth::user()->name }}'s Information</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    <div class="table-responsive">
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Bulstat</th>
+                                    <th>Address</th>
+                                    <th>Email</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{{ Auth::user()->name }}</td>
+                                    <td>{{ Auth::user()->bulstat }}</td>
+                                    <td>{{ Auth::user()->address }}</td>
+                                    <td>{{ Auth::user()->email }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="d-flex justify-content-end">
+                        <button class="btn btn-outline-danger btn-sm me-2">Delete</button>
+                        <button class="btn btn-outline-primary btn-sm">Edit</button>
+                    </div>
                 </div>
             </div>
         </div>
