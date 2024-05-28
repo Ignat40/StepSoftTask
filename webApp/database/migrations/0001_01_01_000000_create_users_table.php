74ef32a3c1acf3dfd625d051fda778071e41a872
Ignat Bojinov <ignatbojinov@gmail.com>
2024-05-28 14:57:09 +0200

    Create and Migrate Databases

--- webApp/database/migrations/0001_01_01_000000_create_users_table.php
+++ webApp/database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('bulstat', 9)->unique();
+            $table->string('address', 250);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
